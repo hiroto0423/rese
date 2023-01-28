619ae9749154b439cf8f4d4d75e1d9bbf00d09dc
tomoyahiroe <tomoya4creative@gmail.com>
2023-01-28 23:49:48 +0900

    feat: basic app

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\ShopsController;
+use App\Http\Controllers\ReservationsController;
+use App\Http\Controllers\FavoriteController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', [ShopsController::class, 'index'])->name('root');
+Route::get('/detail/{shop_id}', [ShopsController::class, 'detail'])->name('shop.detail');
+Route::get('/search', [ShopsController::class, 'search'])->name('shop.search');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/thanks', function () {
+        return view('thanks');
+    });
+
+    Route::get('/mypage', [UsersController::class, 'mypage'])->name('mypage');
+
+    Route::post('/like/{shop_id}', [FavoriteController::class, 'create'])->name('like');
+    Route::post('/unlike/{shop_id}', [FavoriteController::class, 'delete'])->name('unlike');
+
+    Route::post('/reservation', [ReservationsController::class, 'create'])->name('reserve.create');
+    Route::post('/reserve/{reservation_id}', [ReservationsController::class, 'delete'])->name('reserve.delete');
 });
+
+require __DIR__ . '/auth.php';
